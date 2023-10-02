9336a81aa9926ffdcbf6f2bd193cd7c5f3ce9413
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-10-02 23:10:46 +0530

    added loading component

--- law-precedent-bot/src/App.tsx
+++ law-precedent-bot/src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import AddqueryForm from "./components/addqueryform";
+import Main from "./components/main";
+import Loading from "./components/loading";
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900">
-      <AddqueryForm />
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-900">
+      <Main>
+        <AddqueryForm />
+        <Loading />
+      </Main>
     </div>
   );
 }
--- law-precedent-bot/src/components/addqueryform.tsx
+++ law-precedent-bot/src/components/addqueryform.tsx
@@ -1,6 +1,6 @@
 export default function AddqueryForm() {
   return (
-    <form className="mb-20 flex flex-col rounded  p-6 md:flex-row">
+    <form className="flex flex-col rounded  p-6 md:flex-row">
       <div className="mb-4 flex-grow md:mr-4">
         <label
           className="mb-2 block text-sm font-bold text-gray-700"
@@ -11,11 +11,11 @@ export default function AddqueryForm() {
         <input
           id="queryInput"
           type="text"
-          className="w-full rounded border bg-inherit px-3 py-2 text-white focus:outline-none  md:w-96"
+          className="w-full rounded border bg-inherit px-3 py-2 text-white focus:outline-none md:w-96"
         />
       </div>
       <button
-        className="mb-6 mt-7 rounded bg-green-500 px-4 py-2 font-bold text-white md:w-24"
+        className="mb-4 mt-7 rounded bg-green-500 px-3 py-1 font-bold text-white md:w-24"
         type="submit"
       >
         Find
